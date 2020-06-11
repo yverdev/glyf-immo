beed31dbe9215b4e9e322190d8c70bd6f8d5c416
Fred <frederick.vu.pro@gmail.com>
2020-06-11 15:08:43 +0200

    repare menu mobile

--- public/js/main.js
+++ public/js/main.js
@@ -239,7 +239,7 @@ jQuery(document).ready(function($) {
 	// navigation
   var OnePageNavigation = function() {
     var navToggler = $('.site-menu-toggle');
-   	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
+   	$("body").on("click", ".main-menu li a[href^='{{ path('')}} '], .smoothscroll[href^='{{ path('')}}'], .site-mobile-menu .site-nav-wrap li a", function(e) {
       e.preventDefault();
 
       var hash = this.hash;
